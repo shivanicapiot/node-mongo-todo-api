99c715b14a26fe0c58314d59b12525b01b707587
Shivani Dubey <shivani@capiot.com>
2017-09-19 16:52:37 +0530

    added getbyid

--- server/server.js
+++ server/server.js
@@ -52,6 +52,22 @@ app.get('/todos/:id', (req, res) => {
     }
 
 })
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (ObjectID.isValid(id)) {
+        Todo.findByIdAndRemove(id).then((todo) => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+            res.status(200).send(todo)
+        }, (err) => {
+            res.status(404).send()
+        })
+    } else {
+        return res.status(400).send("Invalid ID")
+    }
+
+})
 app.listen(4000, () => {
     console.log("Server Started On Port 4000");
 })
