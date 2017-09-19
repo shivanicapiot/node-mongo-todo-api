e7a35b6f96f5ea7cff45e71e741afccef48334c2
Shivani Dubey <shivani@capiot.com>
2017-09-19 15:04:56 +0530

    added post api

---
+++ server/server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+
+var {
+    mongoose
+} = require('./db/mongoose')
+var {
+    user
+} = require('./models/user');
+var {
+    Todo
+} = require('./models/todo');
+
+var app = express();
+app.use(bodyParser.json());
+app.post('/todos', (req, res) => {
+    var newTodo = new Todo({
+        text: req.body.text
+    })
+    newTodo.save().then((result) => {
+        res.status(200).send(result)
+    }, (err) => {
+        res.status(400).send(err);
+    })
+})
+app.listen(4000, () => {
+    console.log("Server Started On Port 4000");
+})
